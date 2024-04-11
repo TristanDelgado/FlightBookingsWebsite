59bacce33a656f400cc00e08630737818c00fa0a
Delgado <tristandelgado44@gmail.com>
2024-04-11 15:10:55 -0500

    Added most front end UI

--- javascript/indexjs.js
+++ javascript/indexjs.js
@@ -1,1 +1,32 @@
-//Javacript comments
+function minusPassenger() 
+{
+    var doc = document.getElementById("numPassengers");
+    var num = parseInt(doc.value);
+    if(num > 1)
+    {
+        doc.value = num - 1;
+    }
+    doc.innerHTML = doc.value;
+}
+
+function plusPassenger()
+{
+    var doc = document.getElementById("numPassengers");
+    var num = parseInt(doc.value);
+    doc.value = num + 1;
+    doc.innerHTML = doc.value;
+}
+
+function login()
+{
+    var doc = document.getElementById("loginform");
+    doc.style.display = "block";
+}
+
+function signup()
+{
+    var doc = document.getElementById("loginform");
+    doc.style.display = "none";
+    var doc2 = document.getElementById("signupform");
+    doc2.style.display = "block";
+}
